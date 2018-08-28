4464b2c2fe71496fd4c02a884b748a8d08137fdc
Umair Tahir <professor.ut.786@gmail.com>
2018-08-28 11:22:36 +0500

    Setup admin layout

--- SMSProject/App_Start/BundleConfig.cs
+++ SMSProject/App_Start/BundleConfig.cs
@@ -14,18 +14,59 @@ namespace SMSProject
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/customScripts").Include(
+                      "~/Scripts/bars.js",
+                      "~/Scripts/circles.js",
+                      "~/Scripts/contact_me.js",
+                      "~/Scripts/flipclock.js",
+                      "~/Scripts/jqBootstrapValidation.js",
+                      "~/Scripts/jQuery-plugin-progressbar.js",
+                      "~/Scripts/jquery.basictable.min.js",
+                      "~/Scripts/jquery.nicescroll.js",
+                      "~/Scripts/js",
+                      "~/Scripts/monthly.js",
+                      "~/Scripts/prettymaps.js",
+                      "~/Scripts/classie.js",
+                       "~/Scripts/gnmenu.js",
+                      "~/Scripts/screenfull.js",
+                      "~/Scripts/scripts.js",
+                      "~/Scripts/skycons.js",
+                      "~/Scripts/valida.*",
+                      "~/Scripts/validator.min.js"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/bundles/amcharts").Include(
+                "~/Scripts/amcharts.js",
+                "~/Scripts/serial.js",
+                "~/Scripts/light.js",
+                "~/Scripts/export.js",
+                "~/Scripts/Chart.min.js",
+               "~/Scripts/pie.js",
+               "~/Scripts/gauge.js",
+               "~/Scripts/radar.js",
+               "~/Scripts/xy.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/style.css",
+                      "~/Content/basictable.css",
+                      "~/Content/circles.css",
+                      "~/Content/component.css",
+                      "~/Content/export.css",
+                      "~/Content/flipclock.css",
+                      "~/Content/jQuery-plugin-progressbar1.css",
+                      "~/Content/monthly.css",
+                      "~/Content/style_grid.css",
+                      "~/Content/table-style.css"));
+
+            bundles.Add(new StyleBundle("~/Content/fonts").Include(
+                "~/Content/font-awesome.css"));
         }
     }
 }
