0b6b91d63a79bc7bd9b8c6fc9bc21de96f1ea8ca
Umair Tahir <professor.ut.786@gmail.com>
2018-08-28 19:30:31 +0500

    Static Dashboard Design Done

---
+++ SMSProject/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMSProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return RedirectToAction("Dashboard");
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
