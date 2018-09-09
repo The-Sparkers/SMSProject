d92218203edb902672609d3cdf398108e8c2240f
Umair Tahir <professor.ut.786@gmail.com>
2018-09-09 18:04:27 +0500

    Added jquery.min.js file in the bundles

--- SMSProject/App_Start/BundleConfig.cs
+++ SMSProject/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace SMSProject
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                         "~/Scripts/jquery-{version}.min.js",
+                        "~/Scripts/jquery-1.10.2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"
